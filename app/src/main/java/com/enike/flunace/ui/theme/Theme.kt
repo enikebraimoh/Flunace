0679b13b63afb9197206f9ff17b7327964770c59
enikebraimoh <richardbraimoh@gmail.com>
2022-02-04 01:49:39 +0100

    Done with Theming 🤩

--- app/src/main/java/com/enike/flunace/ui/theme/Theme.kt
+++ app/src/main/java/com/enike/flunace/ui/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-
 private val DarkColorPalette = darkColors(
     primary = Orange800,
     primaryVariant = Orange900,
