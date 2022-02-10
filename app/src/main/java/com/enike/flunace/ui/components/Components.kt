c8c05a59390d2fd5e170edfc965ddd808b0f9978
enikebraimoh <richardbraimoh@gmail.com>
2022-02-10 23:25:56 +0100

    hoisted composables and made them stateless

--- app/src/main/java/com/enike/flunace/ui/components/Components.kt
+++ app/src/main/java/com/enike/flunace/ui/components/Components.kt
@@ -1,18 +1,21 @@
 package com.enike.flunace.ui.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
-
-
+import com.enike.flunace.ui.theme.FlunaceTheme
+import com.enike.flunace.ui.theme.myColor
 
 @Composable
 fun DefaultButton(
@@ -20,10 +23,12 @@ fun DefaultButton(
     modifier: Modifier = Modifier
         .fillMaxWidth()
         .padding(vertical = 40.dp),
-    buttonClicked: () -> Unit
+    buttonClicked: () -> Unit,
+    isEnabled : Boolean =  true,
 ) {
     Button(
         modifier = modifier,
+        enabled = isEnabled,
         elevation = ButtonDefaults.elevation(
             defaultElevation = 0.dp,
             pressedElevation = 8.dp,
@@ -39,4 +44,57 @@ fun DefaultButton(
         )
     }
 
+}
+
+
+@Composable
+fun CustomInputField(
+    modifier: Modifier = Modifier
+        .padding(5.dp)
+        .fillMaxWidth(),
+    text: String,
+    onTextChange: (String) -> Unit,
+    shapes: Shape = MaterialTheme.shapes.small.copy(all = CornerSize(15.dp)),
+    leadingIcon: @Composable () -> Unit,
+    hint : String = "8140252210") {
+    Surface(color = MaterialTheme.colors.myColor, shape = shapes) {
+        TextField(
+            modifier = modifier.padding(horizontal = 20.dp, vertical = 5.dp),
+            value = text,
+            onValueChange = onTextChange,
+            textStyle = MaterialTheme.typography.subtitle1,
+            placeholder = {
+                Text(
+                text = hint,
+                style = MaterialTheme.typography.subtitle1,
+            )},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            colors = TextFieldDefaults.textFieldColors(
+                disabledTextColor = Color.Transparent,
+                backgroundColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            leadingIcon = leadingIcon
+        )
+    }
+
+}
+
+@Preview()
+@Preview(name = "default preview", showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultPreview() {
+    FlunaceTheme {
+        CustomInputField(
+            text = "40890990909",
+            onTextChange = {},
+            leadingIcon = {
+                Text(
+                    text = "+234",
+                    style = MaterialTheme.typography.subtitle1,
+                )
+            })
+    }
 }
