5307306d75d8237ddc1f5683d1b20559a127124b
enikebraimoh <richardbraimoh@gmail.com>
2022-02-18 08:50:08 +0100

    added the otp view ✉️

--- app/src/main/java/com/enike/flunace/ui/components/Components.kt
+++ app/src/main/java/com/enike/flunace/ui/components/Components.kt
@@ -1,20 +1,31 @@
 package com.enike.flunace.ui.components
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusOrder
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.enike.flunace.ui.theme.FlunaceTheme
 import com.enike.flunace.ui.theme.myColor
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun DefaultButton(
@@ -86,6 +97,157 @@ fun CustomInputField(
 
 }
 
+@Composable
+fun OTPTextFields(
+    modifier: Modifier = Modifier,
+    length: Int,
+    onFilled: (code: String) -> Unit
+) {
+    var code: List<Char> by remember { mutableStateOf(listOf()) }
+    val focusRequesters: List<FocusRequester> = remember {
+        val temp = mutableListOf<FocusRequester>()
+
+        repeat(length) {
+            temp.add(FocusRequester())
+        }
+        temp
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.height(50.dp)
+    ) {
+        (0 until length).forEach { index ->
+            TextField(
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(50.dp)
+                    .focusOrder(focusRequester = focusRequesters[index]) {
+                        focusRequesters[index + 1].requestFocus()
+                    },
+                textStyle = MaterialTheme.typography.subtitle1.copy(
+                    textAlign = TextAlign.Center, color = MaterialTheme.colors.onBackground
+                ),
+                singleLine = true,
+                value = code.getOrNull(index = index)?.takeIf {
+                    it.isDigit()
+                }?.toString() ?: "",
+                onValueChange = { value: String ->
+                    if (focusRequesters[index].freeFocus()) {
+                        val temp = code.toMutableList()
+                        if (value == "") {
+                            if (temp.size > index) {
+                                temp.removeAt(index = index)
+                                code = temp
+                                focusRequesters.getOrNull(index - 1)?.requestFocus()
+                            }
+                        } else {
+                            if (code.size > index) {
+                                temp[index] = value.getOrNull(0) ?: ' '
+                            } else {
+                                temp.add(value.getOrNull(0) ?: ' ')
+                                code = temp
+                                focusRequesters.getOrNull(index + 1)?.requestFocus() ?: onFilled(
+                                    code.joinToString(separator = "")
+                                )
+                            }
+                        }
+                    }
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+            )
+
+            Spacer(modifier = Modifier.width(15.dp))
+        }
+    }
+}
+
+@Composable
+fun OtpCell(
+    modifier: Modifier,
+    value: String,
+    isCursorVisible: Boolean = false
+) {
+
+    val scope = rememberCoroutineScope()
+    val (cursorSymbol, setCursorSymbol) = remember { mutableStateOf("") }
+
+    // Cursor blinking logic
+    LaunchedEffect(key1 = cursorSymbol, isCursorVisible) {
+        if (isCursorVisible) {
+            scope.launch {
+                delay(350)
+                setCursorSymbol(if (cursorSymbol.isEmpty()) "|" else "")
+            }
+        }
+    }
+
+    // UI for OTP Character
+    Box(
+        modifier = modifier
+    ) {
+        Text(
+            text = if (isCursorVisible) cursorSymbol else value,
+            style = MaterialTheme.typography.body1,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun OtpBugView() {
+    val (editValue, setEditValue) = remember { mutableStateOf("") }
+    val otpLength = remember { 4 }
+    val focusRequester = remember { FocusRequester() }
+    val keyboard = LocalSoftwareKeyboardController.current
+
+    // hidden TextField for controlling OTP Cells UI
+    TextField(
+        value = editValue,
+        onValueChange = {
+            if (it.length <= otpLength) {
+                setEditValue(it)
+            }
+        },
+        modifier = Modifier
+            .size(0.dp)
+            .focusRequester(focusRequester),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number
+        )
+    )
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        (0 until otpLength).map { index ->
+            OtpCell(
+                modifier = Modifier
+                    .size(36.dp)
+                    .clickable {
+                        focusRequester.requestFocus()
+                        // this is required so if keyboard is dissmissed
+                        // then could be open again with focus
+                        keyboard?.show()
+                    }
+                    .border(1.dp, Color.DarkGray),
+                value = editValue.getOrNull(index)?.toString() ?: "",
+                isCursorVisible = editValue.length == index
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+        }
+    }
+
+}
+
+
+//PREVIEWS
+
 @Preview()
 @Preview(name = "default preview", showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
@@ -102,3 +264,21 @@ fun DefaultPreview() {
             })
     }
 }
+
+@Preview(name = "Button preview")
+@Composable
+fun DefaultButtonPreview() {
+    FlunaceTheme {
+        DefaultButton(buttonText = "Button", buttonClicked = {})
+    }
+}
+
+@ExperimentalComposeUiApi
+@Preview(name = "otp preview", showBackground = true)
+@Composable
+fun DefaultOtpPreview() {
+    FlunaceTheme {
+        OtpBugView()
+    }
+}
+
