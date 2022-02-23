9c2dc7d98c8a87e080c20e6399604cf1850a5033
enikebraimoh <richardbraimoh@gmail.com>
2022-02-23 18:49:10 +0100

    added google Map composable ⛳️

--- app/src/main/java/com/enike/flunace/ui/picklocation/PickLocation.kt
+++ app/src/main/java/com/enike/flunace/ui/picklocation/PickLocation.kt
@@ -1,33 +1,130 @@
 package com.enike.flunace.ui.picklocation
 
+import android.content.Context
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.enike.flunace.ui.theme.FlunaceTheme
 import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+
+import androidx.core.content.ContextCompat
+
+import android.graphics.drawable.Drawable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.platform.LocalContext
+
+import com.google.android.gms.maps.model.BitmapDescriptor
+
+
+
 
 
 @Composable
 fun Map() {
 
-    val singapore = LatLng(1.35, 103.87)
-
-    GoogleMap(modifier = Modifier.fillMaxSize(),
-        googleMapOptionsFactory = {
-            GoogleMapOptions().camera(CameraPosition.fromLatLngZoom(singapore, 11f))
-        }) {
-        Marker(
-            position = singapore,
-            title = "singapore",
-            snippet = "marker in singapore"
-        )
+    val nigeria = LatLng(9.0820, 8.6753)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(nigeria, 10f)
     }
+    val (text, setText) = remember { mutableStateOf("") }
+    val context = LocalContext.current
+    val resources = LocalContext.current.resources
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        GoogleMap(
+            googleMapOptionsFactory = {
+                GoogleMapOptions().camera(CameraPosition.fromLatLngZoom(nigeria, 10f))
+            }, uiSettings = MapUiSettings(zoomControlsEnabled = false),
+            cameraPositionState = cameraPositionState
+        ){
+            Marker(position = nigeria, title = "Nigeria",
+                icon = BitmapFromVector(context = context, vectorResId = com.enike.flunace.R.drawable.ic_location) )
+        }
+
+        Box(modifier = Modifier.padding(all = 20.dp)) {
+            Surface(
+                color = Color.White,
+                shape = MaterialTheme.shapes.small.copy(all = CornerSize(15.dp))
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp, vertical = 5.dp)
+                        .fillMaxWidth(),
+                    value = text,
+                    onValueChange = setText,
+                    textStyle = MaterialTheme.typography.subtitle1,
+                    placeholder = {
+                        Text(
+                            color = Color.Gray,
+                            text = "Search for a location",
+                            style = MaterialTheme.typography.subtitle1,
+                        )
+                    },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
+                    colors = TextFieldDefaults.textFieldColors(
+                        disabledTextColor = Color.Transparent,
+                        backgroundColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent
+                    )
+                )
+            }
+        }
+
+    }
+
+
+}
+
+private fun BitmapFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+    // below line is use to generate a drawable.
+    val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
+
+    // below line is use to set bounds to our vector drawable.
+    vectorDrawable!!.setBounds(0, 0, vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight)
+
+    // below line is use to create a bitmap for our
+    // drawable which we have added.
+    val bitmap = Bitmap.createBitmap(
+        vectorDrawable.intrinsicWidth,
+        vectorDrawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
+    )
+
+    // below line is use to add bitmap in our canvas.
+    val canvas = Canvas(bitmap)
+
+    // below line is use to draw our
+    // vector drawable in canvas.
+    vectorDrawable.draw(canvas)
+
+    // after generating our bitmap we are returning our bitmap.
+    return BitmapDescriptorFactory.fromBitmap(bitmap)
 }
 
 
