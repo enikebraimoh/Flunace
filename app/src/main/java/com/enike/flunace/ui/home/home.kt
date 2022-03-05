bb11f22bbd1f84459810e98b31adb251dcb8b116
enikebraimoh <richardbraimoh@gmail.com>
2022-03-05 04:16:17 +0100

    fixing bottom navigation bar

--- app/src/main/java/com/enike/flunace/ui/home/home.kt
+++ app/src/main/java/com/enike/flunace/ui/home/home.kt
@@ -1,5 +1,7 @@
 package com.enike.flunace.ui.home
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -8,8 +10,11 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.enike.flunace.R
 import com.enike.flunace.ui.theme.FlunaceTheme
 
 @Composable
@@ -23,6 +28,7 @@ fun Home() {
             FloatingActionButton(onClick = {
 
             }, shape = RoundedCornerShape(50)) {
+                Icon(painter = painterResource(id = R.drawable.ic_vendor), contentDescription ="" )
 
             }
         },
@@ -43,6 +49,36 @@ fun Home() {
                             },
                             icon = {
                                 Icon(imageVector = Icons.Default.Home, contentDescription = "")
+                            },
+                            label = {
+                                Text(text = "Home")
+                            },
+                            alwaysShowLabel = false
+                        )
+                        BottomNavigationItem(
+                            selected = selectedItem.value == "history",
+                            onClick = {
+                                selectedItem.value = "home"
+                                content.value = "Home Screen"
+                            },
+                            icon = {
+                                Icon(painter = painterResource(id = R.drawable.ic_history_icon), contentDescription ="" )
+                            },
+                            label = {
+                                Text(text = "Home")
+                            },
+                            alwaysShowLabel = false
+                        )
+                        Spacer(modifier = Modifier.width(50.dp))
+                        BottomNavigationItem(
+                            selected = selectedItem.value == "cart",
+                            onClick = {
+                                selectedItem.value = "home"
+                                content.value = "Home Screen"
+                            },
+                            icon = {
+
+                                Icon(painter = painterResource(id = R.drawable.ic_cart_icon), contentDescription ="" )
                             },
                             label = {
                                 Text(text = "Home")
