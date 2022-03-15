4d1bf16cd6faee845675a406f2a718df4591f173
enikebraimoh <richardbraimoh@gmail.com>
2022-03-15 01:15:59 +0100

    SOLID principle ✅

---
+++ domain/src/main/java/com/enike/domain/model/VerifyPhoneNumberResponse.kt
@@ -0,0 +1,3 @@
+package com.enike.domain.model
+
+data class VerifyPhoneNumberResponse(val status: String, val message: String, val data: Any)
--- domain/src/main/java/com/enike/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/enike/domain/repository/AuthRepository.kt
@@ -1,5 +1,10 @@
 package com.enike.domain.repository
 
+import com.enike.domain.model.VerifyPhoneNumberResponse
+import com.enike.domain.usecase.VerifyPhoneNumber
+
 interface AuthRepository {
+
+    suspend fun verifyPhoneNumber(phoneNumber: VerifyPhoneNumber) : VerifyPhoneNumberResponse
 
 }
