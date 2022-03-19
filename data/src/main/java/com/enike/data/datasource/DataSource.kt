1a4459b33aaafa5d3a7707193bc5b29a852bb83e
enikebraimoh <richardbraimoh@gmail.com>
2022-03-19 15:53:26 +0100

    DataSource fix

--- data/src/main/java/com/enike/data/datasource/DataSource.kt
+++ data/src/main/java/com/enike/data/datasource/DataSource.kt
@@ -3,5 +3,9 @@ package com.enike.data.datasource
 import com.enike.domain.model.VerifyPhoneNumberResponse
 
 interface DataSource {
+
     suspend fun verifyPhoneNumber(phone : String) : VerifyPhoneNumberResponse
+
+
+
 }
