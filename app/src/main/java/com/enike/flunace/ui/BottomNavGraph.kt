d07967d1f6658c2f8f1e536fe712209e14c4d92c
enikebraimoh <richardbraimoh@gmail.com>
2022-04-03 11:56:22 +0100

    added bottom navigation bar ✨

---
+++ app/src/main/java/com/enike/flunace/ui/BottomNavGraph.kt
@@ -0,0 +1,28 @@
+package com.enike.flunace.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.enike.flunace.ui.components.ExploreScreen
+
+@Composable
+fun BottomNavGraph(navController: NavHostController) {
+
+    NavHost(navController = navController, startDestination = BottomBarScreen.Home.route) {
+        composable(BottomBarScreen.Explore.route) {
+            ExploreScreen()
+        }
+        composable(BottomBarScreen.History.route) {
+            ExploreScreen()
+        }
+        composable(BottomBarScreen.Cart.route) {
+            ExploreScreen()
+        }
+        composable(BottomBarScreen.Home.route) {
+            ExploreScreen()
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/enike/flunace/ui/components/Explore.kt
@@ -0,0 +1,18 @@
+package com.enike.flunace.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ExploreScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+    )
+
+}
